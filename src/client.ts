a56dc1d5f87bc358a57d826ce9a86ea90bf2b99e
Daniel Emery <daniel@sine.co>
2020-08-10 17:25:53 +0930

    Bumped client to use new models

--- src/client.ts
+++ src/client.ts
@@ -6,7 +6,6 @@ import {
   GrpcTypes,
   ServiceTypes,
 } from '@teamest/internal-season-common';
-import { TeamSeason } from '@teamest/models/processed';
 
 export default class InternalSeasonClient {
   client: GrpcServices.SeasonClient;
@@ -19,18 +18,38 @@ export default class InternalSeasonClient {
   }
 
   async updateTeamSeason(
-    teamSeason: TeamSeason,
-  ): Promise<ServiceTypes.UpdateTeamSeasonResult> {
-    const packagedTeamSeason = GrpcPackaging.packageTeamSeason(teamSeason);
+    updateTeamSeasonRequest: ServiceTypes.UpdateTeamSeasonRequest,
+  ): Promise<ServiceTypes.UpdateTeamSeasonResponse> {
+    const packagedRequest = GrpcPackaging.packageUpdateTeamSeasonRequest(updateTeamSeasonRequest);
     return new Promise((resolve, reject) => {
       this.client.updateTeamSeason(
-        packagedTeamSeason,
-        (err, result: GrpcTypes.UpdateTeamSeasonResult | undefined) => {
+        packagedRequest,
+        (err, result: GrpcTypes.UpdateTeamSeasonResponse | undefined) => {
           if (err) reject(err);
           if (!result) {
             reject('No result');
           } else {
-            const unpacked = GrpcPackaging.unpackageUpdateTeamSeasonResult(
+            const unpacked = GrpcPackaging.unpackageUpdateTeamSeasonResponse(
+              result,
+            );
+            resolve(unpacked);
+          }
+        },
+      );
+    });
+  }
+
+  async getSeasonsForTeam(getSeasonsForTeamRequest: ServiceTypes.GetSeasonsForTeamRequest): Promise<ServiceTypes.GetSeasonsForTeamResponse> {
+    const packagedRequest = GrpcPackaging.packageGetSeasonsForTeamRequest(getSeasonsForTeamRequest);
+    return new Promise((resolve, reject) => {
+      this.client.getSeasonsForTeam(
+        packagedRequest,
+        (err, result: GrpcTypes.GetSeasonsForTeamResponse | undefined) => {
+          if (err) reject(err);
+          if (!result) {
+            reject('No result');
+          } else {
+            const unpacked = GrpcPackaging.unpackageGetSeasonsForTeamResponse(
               result,
             );
             resolve(unpacked);
