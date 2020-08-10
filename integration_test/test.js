c397bb1e97088a45265903c6afa89c164c027075
Daniel Emery <daniel@sine.co>
2020-08-10 21:07:21 +0930

    Bumped package versions

--- integration_test/test.js
+++ integration_test/test.js
@@ -4,13 +4,45 @@ const { InternalSeasonClient } = package;
 
 (async () => {
   const testClient = new InternalSeasonClient('localhost:50051');
-  const result = await testClient.updateTeamSeason({
-    events: [],
-    matchDuration: 55,
-    seasonName: 'Season 1',
-    teamName: 'My Team',
-    timeScraped: new Date(),
-    timezone: 'Australia/Adelaide',
+  const updateResult = await testClient.updateTeamSeason({
+    teamSeason: {
+      events: [
+        {
+          type: 'match',
+          time: new Date('2020-06-05T20:30:00Z'),
+          duration: 60,
+          timezone: 'Australia/Adelaide',
+          venue: 'Thebarton Senior College',
+          court: 'Court 5',
+          home: {
+            name: 'Dateko',
+            isExternal: false,
+          },
+          away: {
+            name: 'Karasuno',
+            isExternal: false,
+          },
+          duty: undefined,
+          round: 'Round 1',
+        },
+      ],
+      competitionName: 'Test Competition Please Ignore',
+      seasonName: 'Season 1',
+      teamName: 'My Team',
+      lastScraped: new Date(),
+    },
   });
-  console.log(result);
+  console.log('Update Result');
+  console.log(JSON.stringify(updateResult));
+  const readResult = await testClient.getSeasonsForTeam({
+    teamSpecifiers: [
+      {
+        competitionName: 'Test Competition Please Ignore',
+        seasonName: 'Season 1',
+        teamName: 'My Team',
+      },
+    ],
+  });
+  console.log('Read Result');
+  console.log(JSON.stringify(readResult));
 })();
